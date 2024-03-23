a4d03095284f463791e43793dd792289889ffdd9
zane <39070793+zaneenders@users.noreply.github.com>
2024-03-23 10:05:23 -0600

    renamed Client to TerminalClient

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "Scribe",
     products: [
-        .executable(name: "Client", targets: ["Client"]),
+        .executable(name: "Client", targets: ["TerminalClient"]),
         .executable(name: "Server", targets: ["Server"]),
         .library(name: "Scribe", targets: ["Scribe"]),
     ],
@@ -17,7 +17,7 @@ let package = Package(
         // Examples
         .target(name: "Programs", dependencies: ["Scribe"]),
         .executableTarget(
-            name: "Client",
+            name: "TerminalClient",
             dependencies: ["Scribe"],
             swiftSettings: [
                 .enableUpcomingFeature("BareSlashRegexLiterals"),
